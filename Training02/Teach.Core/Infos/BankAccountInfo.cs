3814da8a44363cf246e7e7408891ad3283ced0e2
曾彥 <Antony77@tecyt.com>
2016-12-03 17:42:25 +0800

    1.CSV?!

--- Training02/Teach.Core/Infos/BankAccountInfo.cs
+++ Training02/Teach.Core/Infos/BankAccountInfo.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TEC.Core.Text.Format;
 
 namespace Teach.Core.Infos
 {
     public class BankAccountInfo
     {
+        [OrderedFormatMember(0),DisplayName("帳號ID")]
         public Guid BankAccountId { set; get; }
+        [OrderedFormatMember(1),DisplayName("使用者ID")]
         public Guid BankUserId { set; get; }
+        [OrderedFormatMember(2),DisplayName("摳摳")]
         public decimal Amount { set; get; }
     }
 }
--- Training02/Teach.Web/Controllers/HomeController.cs
+++ Training02/Teach.Web/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using Teach.Core;
 using Teach.Core.Infos;
 using Teach.Core.Task;
 using Teach.Web.Models.Home;
+using TEC.Core.Text.Format;
 using TEC.Core.Text.RandomText;
 using TEC.Core.Transactions;
 
@@ -49,6 +50,39 @@ namespace Teach.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            List<BankAccountInfo> dataSource = new List<BankAccountInfo>()
+            {
+                new BankAccountInfo()
+                {
+                     Amount=500,
+                     BankAccountId=Guid.NewGuid(),
+                     BankUserId = Guid.NewGuid()
+                },
+                new BankAccountInfo()
+                {
+                     Amount=400,
+                     BankAccountId=Guid.NewGuid(),
+                     BankUserId = Guid.NewGuid()
+                }
+            };
+            string firstLine = OrderedMemberCSVFormatter.serializeAttribute<BankAccountInfo, System.ComponentModel.DisplayNameAttribute>(t => t.DisplayName, ",");
+
+            string multiRecordResult = firstLine + Environment.NewLine + String.Join(Environment.NewLine,
+              dataSource.Select(t => OrderedMemberCSVFormatter.serializeObject(t, ",", (propertyInfo, value) =>
+              {
+                  if (propertyInfo.PropertyType == typeof(Guid))
+                  {
+                      //force to specific format
+                      return ((Guid)value).ToString("N");
+                  }
+                  if (value == null)
+                  {
+                      return String.Empty;
+                  }
+                  //default to object string 
+                  return value.ToString();
+              })));
+
             //SequentialTransactionManager sequentialTransactionManager = new SequentialTransactionManager();
             //sequentialTransactionManager.EnlistmentNotificationCollection.Add(new LoginTransactionTask());
             ////sequentialTransactionManager.setContextValue("accountId", accountId);
